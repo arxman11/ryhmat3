f84c34456c31d14767325b26e7bd256dee621e7a
Erdnah <handreelias@gmail.com>
2014-04-16 16:11:51 +0300

    Some tweaks

--- src/ee/oop/mang/Main.java
+++ src/ee/oop/mang/Main.java
@@ -9,40 +9,49 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
+
 	String[] map = {"                ",
-					"                ",
 					"#.#. ..         ",
-					"##### ##########"};
+					"##### ##########" };
 	private int canvasW = 800;
-	private int canvasH = 600;
-	
-	final private int size = 50;
+	private static int canvasH = 600;
+
+	final private static int size = 50;
+
+	static GraphicsContext g;
 
 	@Override
 	public void start(Stage stage) {
-		
+
 		Canvas canvas = new Canvas(canvasW, canvasH);
-		GraphicsContext g = canvas.getGraphicsContext2D();
+		g = canvas.getGraphicsContext2D();
 		g.setFill(Color.BLACK);
 		g.fillRect(0, 0, canvasW, canvasH);
 		g.fill();
+
+		loadMap(map);
+
+		Group juur = new Group(canvas);
+		stage.setScene(new Scene(juur));
+		stage.show();
+
+	}
+
+	public static void loadMap(String[] map) {
 		for (int i = 0; i < map.length; i++) {
 			for (int j = 0; j < map[0].length(); j++) {
 				char curC = map[i].charAt(j);
 				if (curC == '#') {
 					g.setFill(Color.GREEN);
-					g.fillRect(j * size, i * size + canvasH-size*map.length, size, size);
+					g.fillRect(j * size,
+							i * size + canvasH - size * map.length, size, size);
 				} else if (curC == '.') {
 					g.setFill(Color.RED);
-					g.fillOval(j * size, i * size + canvasH-size*map.length, size, size);
+					g.fillOval(j * size,
+							i * size + canvasH - size * map.length, size, size);
 				}
 			}
 		}
-		Group juur = new Group(canvas);
-		stage.setScene(new Scene(juur));
-		stage.show();
-		
 	}
 
 	public static void main(String[] args) {
