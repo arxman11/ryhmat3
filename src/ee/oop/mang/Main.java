c6a462e42067cabbd5d5aeb789fd073600c0232f
Erdnah <handreelias@gmail.com>
2014-04-21 14:19:13 +0300

    asd

--- src/ee/oop/mang/Main.java
+++ src/ee/oop/mang/Main.java
@@ -1,60 +1,57 @@
 package ee.oop.mang;
 
+import java.util.Random;
+
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Main extends Application {
 
-	String[] map = {"                ",
-					"#.#. ..         ",
-					"##### ##########" };
-	private int canvasW = 800;
-	private static int canvasH = 600;
-
-	final private static int size = 50;
-
-	static GraphicsContext g;
+	Random rand = new Random();
 
 	@Override
-	public void start(Stage stage) {
-
-		Canvas canvas = new Canvas(canvasW, canvasH);
-		g = canvas.getGraphicsContext2D();
-		g.setFill(Color.BLACK);
-		g.fillRect(0, 0, canvasW, canvasH);
-		g.fill();
-
-		loadMap(map);
-
-		Group juur = new Group(canvas);
-		stage.setScene(new Scene(juur));
-		stage.show();
-
-	}
-
-	public static void loadMap(String[] map) {
-		for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map[0].length(); j++) {
-				char curC = map[i].charAt(j);
-				if (curC == '#') {
-					g.setFill(Color.GREEN);
-					g.fillRect(j * size,
-							i * size + canvasH - size * map.length, size, size);
-				} else if (curC == '.') {
-					g.setFill(Color.RED);
-					g.fillOval(j * size,
-							i * size + canvasH - size * map.length, size, size);
+	public void start(final Stage lava) {
+		Group juur = new Group();
+		final Scene scene = new Scene(juur, 1000, 500);
+		
+		final int radius = 25;
+		
+		for (int i = 0; i < 1000; i++) {
+			final Circle circle = new Circle(radius);
+			int x = rand.nextInt((int) (scene.getWidth()-radius));
+			int y = rand.nextInt((int) (scene.getHeight()-radius));
+			TranslateTransition tt = new TranslateTransition(new Duration(5000), circle);
+			tt.setFromY(y-scene.getHeight());
+			tt.setToY(y);
+			circle.setLayoutX(x);
+			circle.setLayoutY(y);
+			//tt.play();
+			circle.setFill(Color.rgb(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255)));
+			circle.setOnMouseEntered(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					int x = rand.nextInt((int) (scene.getWidth()-radius));
+					int y = rand.nextInt((int) (scene.getHeight()-radius));
+					circle.setLayoutX(x);
+					circle.setLayoutY(y);
 				}
-			}
+			});
+			juur.getChildren().add(circle);
 		}
+		lava.setScene(scene);
+		lava.show();
 	}
 
 	public static void main(String[] args) {
 		launch(args);
 	}
 }
+
